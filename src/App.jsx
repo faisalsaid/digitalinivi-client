b38d3274b99104cd7bc0a23e9886558995ec22d2
faisalsaid <falsdesain@gmail.com>
2023-12-05 22:34:48 +0900

    init tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 function App() {
- 
   return (
     <>
-    <div>Halo</div>
+      <div className="text-2xl text-red-500">Halo</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
