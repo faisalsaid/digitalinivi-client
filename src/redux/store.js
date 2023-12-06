268395ce5162a29f929392d6c8c2847a84bc42fc
faisalsaid <falsdesain@gmail.com>
2023-12-06 14:40:58 +0900

    add user slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../feature/profile/config/userSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    user: userReducer,
+  },
 });
