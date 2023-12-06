61c30db36f751980d9796bc593137ae7b3e8c2ff
faisalsaid <falsdesain@gmail.com>
2023-12-06 19:30:48 +0900

    add select filter input on tempalte page

--- src/feature/templates/TemplatesComponent.jsx
+++ src/feature/templates/TemplatesComponent.jsx
@@ -1,13 +1,42 @@
-import { Stack, Typography, Card, CardActions, CardContent, CardMedia, Button } from '@mui/material';
-import React from 'react';
+import { Stack, Typography, Card, CardActions, CardContent, CardMedia, Button, Box, InputLabel, MenuItem, FormControl, Select } from '@mui/material';
+import { useState } from 'react';
 
 const TemplatesComponent = () => {
+  const [category, setCategory] = useState('all');
+  const [popular, setPopular] = useState('all');
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+  const handelPopularChange = (event) => {
+    setPopular(event.target.value);
+  };
   return (
     <Stack className="max-w-7xl min-h-screen mx-auto  " padding={2} gap={3}>
       <Typography component={'h4'} variant="h5" textAlign={'center'}>
         Template Undangan
       </Typography>
-      <Stack>hlo</Stack>
+      <Stack direction={'row'} gap={2}>
+        <div className="md:w-52">
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Kategori</InputLabel>
+            <Select labelId="demo-simple-select-label" id="demo-simple-select" value={category} label="Kategori" onChange={handleCategoryChange}>
+              <MenuItem value={'all'}>Semua</MenuItem>
+              <MenuItem value={'wedding'}>Pernikahan</MenuItem>
+              <MenuItem value={'birthday'}>Ulang Tahun</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <div className="md:w-52">
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Populer</InputLabel>
+            <Select labelId="demo-simple-select-label" id="demo-simple-select" value={popular} label="Populer" onChange={handelPopularChange}>
+              <MenuItem value={'all'}>Semua</MenuItem>
+              <MenuItem value={'popular'}>Populer</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </Stack>
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-4">
         {[1, 2, 3, 4, 5, 6, 7].map((data, i) => (
           <Card key={data}>
