1ffb24ac84df5268f60a351252705426eaed72df
faisalsaid <falsdesain@gmail.com>
2023-12-06 13:30:14 +0900

    fix auth page backgorund color

--- src/feature/signup/SignupComponent.jsx
+++ src/feature/signup/SignupComponent.jsx
@@ -9,7 +9,7 @@ const SignupComponent = () => {
     <div className="flex w-full mx-0 h-screen justify-center items-center">
       <div className="sm:max-w-7xl w-full p-4  sm:h-[80%] sm:w-[80%] flex rounded-md overflow-hidden">
         <div className="flex-1 hidden sm:flex bg-sky-600"></div>
-        <div className="sm:flex-1 w-full  flex flex-col gap-2 justify-center items-center  p-4 sm:max-w-[50%]">
+        <div className="sm:flex-1 w-full  flex flex-col gap-2 justify-center items-center bg-white  p-4 sm:max-w-[50%]">
           <div className="border p-4 rounded-md bg-white w-full">
             <h2 className="uppercase text-xl text-center font-semibold mb-4 text-sky-700">Sign Up</h2>
             <form>
