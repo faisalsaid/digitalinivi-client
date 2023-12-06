462e00eacb879c38ede9f403c444cea78ac322c4
faisalsaid <falsdesain@gmail.com>
2023-12-06 14:06:07 +0900

    add dashbaord simle ui

--- src/feature/dashboard/DashboardComponent.jsx
+++ src/feature/dashboard/DashboardComponent.jsx
@@ -1,7 +1,22 @@
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+// Import icons END
+
+import { Button, Stack, Typography } from '@mui/material';
 import React from 'react';
 
 const DashboardComponent = () => {
-  return <div>DashboardComponent</div>;
+  return (
+    <Stack component={'div'}>
+      <div className="flex gap-2 flex-col sm:flex-row sm:justify-between">
+        <Typography component={'h3'} variant="h5" fontWeight={400} color={'darkslategray'}>
+          Hi, User
+        </Typography>
+        <Button variant="contained" startIcon={<AddBusinessIcon />}>
+          Create Store
+        </Button>
+      </div>
+    </Stack>
+  );
 };
 
 export default DashboardComponent;
