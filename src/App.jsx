0ad7c19ec2eeb36c6ee0162b6149476b3909422b
faisalsaid <falsdesain@gmail.com>
2023-12-06 13:33:05 +0900

    add dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { printLog } from './hook/helper';
 import PageNotFound from './components/template/pageNotFound';
 import SigninComponent from './feature/signin/SigninComponent';
 import SignupComponent from './feature/signup/SignupComponent';
+import DashboardComponent from './feature/dashboard/DashboardComponent';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/signin" element={<SigninComponent />} />
           <Route path="/signup" element={<SignupComponent />} />
+          <Route path="/dashboard" element={<DashboardComponent />} />
           <Route path="/*" element={<PageNotFound />} />
         </Routes>
       </Router>
