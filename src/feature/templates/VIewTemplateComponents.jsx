97bbe9ebeba39b9661ffb837f68387a3547f19b6
faisalsaid <falsdesain@gmail.com>
2023-12-07 19:14:59 +0900

    add navigation on bottom menu

--- src/feature/templates/VIewTemplateComponents.jsx
+++ src/feature/templates/VIewTemplateComponents.jsx
@@ -11,11 +11,12 @@ import { FaWhatsapp } from 'react-icons/fa';
 // import icons end
 
 import { Stack, IconButton } from '@mui/material';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useRef } from 'react';
 
 const bottomMenu = [
   {
-    label: 'articel',
+    label: 'cover',
     icon: <ArticleIcon color="success" />,
   },
   {
@@ -41,15 +42,67 @@ const theme = {
 };
 
 const VIewTemplateComponents = () => {
-  const params = useParams();
-  console.log(params);
+  const coverSection = useRef(null);
+  const brideSection = useRef(null);
+  const dateSection = useRef(null);
+  const galerySection = useRef(null);
+  const commentsSection = useRef(null);
+
+  const bottomMenu = [
+    {
+      label: 'cover',
+      icon: <ArticleIcon color="success" />,
+      section: coverSection,
+    },
+    {
+      label: 'bride',
+      icon: <FavoriteIcon color="success" />,
+      section: brideSection,
+    },
+    {
+      label: 'date',
+      icon: <EventIcon color="success" />,
+      section: dateSection,
+    },
+    {
+      label: 'galery',
+      icon: <ImageIcon color="success" />,
+      section: galerySection,
+    },
+    {
+      label: 'comment',
+      icon: <CommentIcon color="success" />,
+      section: commentsSection,
+    },
+  ];
+
+  const scroolToSection = (section) => {
+    window.scrollTo({
+      top: section.current.offsetTop,
+      behavior: 'smooth',
+    });
+  };
+
   return (
-    <div className={`mx-auto max-w-7xl min-h-screen relative bg-green-50 text-green-700`} style={{ fontFamily: 'poppins' }}>
-      <div id="cover" className={` w-full min-h-screen p-2 relative flex justify-center items-center flex-col gap-8`}>
-        <div className="absolute -top-8 -left-14">{topDecoration}</div>
-        <p className={`text-lg font-semibold text-${theme.color}-700 text-center`}>Kami Mengundang Anda ke pernikahan :</p>
-        <div className={`w-52 h-52 bg-green-100 border rounded-3xl`}>
-          <img src="" alt="" />
+    <div className={`mx-auto max-w-7xl min-h-screen relative bg-green-50 text-green-700 overflow-hidden`} style={{ fontFamily: 'poppins' }}>
+      <div className={`fixed bottom-0 p-4 bg-${theme.color}-100 w-full  flex justify-around sm:max-w-lg left-1/2 -translate-x-1/2 sm:rounded-full sm:bottom-4 z-10`}>
+        {bottomMenu.map((menu, i) => (
+          <IconButton onClick={() => scroolToSection(menu.section)} key={menu.label} aria-label={menu.label}>
+            {menu.icon}
+          </IconButton>
+        ))}
+      </div>
+      <div ref={coverSection} id="cover" className={` w-full min-h-screen p-2 py-24 relative flex justify-center items-center flex-col gap-8`}>
+        <div className="absolute -top-[70px]  scale-125">
+          <img src={`/ornamen-${theme.color}.svg`} />
+        </div>
+        <p className={`text-lg font-semibold text-center`}>Kami Mengundang Anda ke pernikahan :</p>
+        <div className={`w-52 h-52 bg-green-100 border rounded-3xl flex flex-grow overflow-hidden`}>
+          <img
+            className="max-h-full min-w-full object-cover align-bottom"
+            src="https://images.unsplash.com/photo-1517456215183-9a2c3a748d0c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
         </div>
         <p className={`  text-center text-6xl `} style={{ fontFamily: 'Great Vibes', fontWeight: 400 }}>
           Budi & Wati
@@ -66,7 +119,7 @@ const VIewTemplateComponents = () => {
       </div>
 
       {/* BRIDE START */}
-      <div id="bride" className={` w-full min-h-screen  relative  justify-center items-center `}>
+      <div ref={brideSection} id="bride" className={` w-full min-h-screen  relative  justify-center items-center `}>
         <div className="px-6 py-14 bg-green-800 text-white flex gap-4 flex-col">
           <p className="text-center">
             “Dan Di Antara Tanda-Tanda (Kebesaran)-Nya Ialah Bahwa Dia Menciptakan Pasangan-Pasangan Untukmu Dari (Jenis) Dirimu Sendiri Agar Kamu Merasa Tenteram KepadaNya. Dia
@@ -78,8 +131,12 @@ const VIewTemplateComponents = () => {
           <p className="text-center">Dengan Memohon Rahmat Dan Ridho Dari Allah SWT. Kami Bermaksud Menyelenggarakan Syukuran Pernikahan Putra Putri Kami :</p>
           <div className="flex flex-col gap-10 sm:flex-row">
             <div className="flex flex-col items-center gap-4">
-              <div className={`w-52 h-52 bg-green-100 border rounded-3xl`}>
-                <img src="" alt="" />
+              <div className={`w-52 h-52 bg-green-100 border rounded-3xl flex flex-grow overflow-hidden`}>
+                <img
+                  className="max-h-full min-w-full object-cover align-bottom"
+                  src="https://images.unsplash.com/photo-1582897291228-f7676bfcd52c?q=80&w=1904&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
               </div>
               <p className="text-center text-4xl" style={{ fontFamily: 'Great Vibes', fontWeight: 400 }}>
                 Ini Budi Pratama S.Pd M.M
@@ -90,8 +147,12 @@ const VIewTemplateComponents = () => {
               </div>
             </div>
             <div className="flex flex-col items-center gap-4">
-              <div className={`w-52 h-52 bg-green-100 border rounded-3xl`}>
-                <img src="" alt="" />
+              <div className={`w-52 h-52 bg-green-100 border flex rounded-3xl flex-grow overflow-hidden `}>
+                <img
+                  className="max-h-full min-w-full object-cover align-bottom"
+                  src="https://images.unsplash.com/photo-1517455850349-65b65b06f255?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt=""
+                />
               </div>
               <p className="text-center text-4xl" style={{ fontFamily: 'Great Vibes', fontWeight: 400 }}>
                 Dr. Ini Wati S.Sos
@@ -105,9 +166,11 @@ const VIewTemplateComponents = () => {
         </div>
       </div>
       {/* BRIDE END */}
-
+      <div>
+        <img className="mx-auto" src="/ornamen-green.svg" />
+      </div>
       {/* LOCATION START */}
-      <div id="location" className="px-4 py-14 flex justify-center flex-col min-h-screen gap-8">
+      <div ref={dateSection} id="date" className="px-4 py-14 flex justify-center flex-col min-h-screen gap-8">
         <p className="text-center">Insya Allah Acara Akan Dilaksanakan Pada :</p>
         <div className="flex flex-col gap-8 sm:flex-row">
           <div className="flex flex-col gap-2 flex-1">
@@ -161,8 +224,11 @@ const VIewTemplateComponents = () => {
         </div>
       </div>
       {/* LOCATION END */}
+      <div>
+        <img className="mx-auto" src="/ornamen-green.svg" />
+      </div>
       {/* GALERY START */}
-      <div id="galery" className="px-4 py-14 flex  flex-col min-h-screen gap-4 ">
+      <div ref={galerySection} id="galery" className="px-4 py-14 flex  flex-col min-h-screen gap-4 ">
         <p className="text-xl">Galeri Photo :</p>
 
         <div className="columns-2 sm:columns-2 md:columns-3 gap-3 ">
@@ -232,14 +298,21 @@ const VIewTemplateComponents = () => {
         </div>
       </div>
       {/* GALERY END */}
+      <div>
+        <img className="mx-auto" src="/ornamen-green.svg" />
+      </div>
       {/* COMENT START */}
-      <div id="comment" className="px-4 py-14 flex  flex-col min-h-screen gap-4 ">
+      <div ref={commentsSection} id="comment" className="px-4 py-14 flex  flex-col min-h-screen gap-4 ">
         <p className="text-center">
           Tiada Yang Dapat Kami Ungkapkan Selain Rasa Terimakasih Dari Hati Yang Tulus Apabila Bapak/ Ibu/ Saudara/i Berkenan Hadir Untuk Memberikan Do’a Restu Kepada Kami
         </p>
         <div className="flex flex-col items-center gap-4">
-          <div className={`w-52 h-52 bg-green-100 border rounded-3xl`}>
-            <img src="" alt="" />
+          <div className={`w-52 h-52 bg-green-100 border rounded-3xl flex flex-grow overflow-hidden`}>
+            <img
+              className="max-h-full min-w-full object-cover align-bottom"
+              src="https://images.unsplash.com/photo-1517456215183-9a2c3a748d0c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
           </div>
           <p className="text-4xl" style={{ fontFamily: 'Great Vibes', fontWeight: 400 }}>
             Budi & Wati
@@ -294,22 +367,17 @@ const VIewTemplateComponents = () => {
       </div>
       {/* COMENT START */}
 
-      <div id="comment" className="px-4 py-14 flex  flex-col h-80 gap-2 bg-green-800 justify-center items-center text-white">
+      <div id="comment" className="px-4 py-14 flex  flex-col h-80 gap-2 bg-green-800 justify-center items-center text-white relative">
+        <div className="absolute -top-[70px]  scale-125">
+          <img src="/ornamen-green.svg" />
+        </div>
         <p className="text-xl">Vendor Name</p>
         <p className="flex gap-2 items-center">
           <FaWhatsapp /> <span>08123456789</span>
         </p>
       </div>
 
-      <div className="h-[75px]"></div>
-
-      <div className={`fixed bottom-0 p-4 bg-${theme.color}-100 w-full  flex justify-around sm:max-w-lg left-1/2 -translate-x-1/2 sm:rounded-full sm:bottom-4`}>
-        {bottomMenu.map((menu, i) => (
-          <IconButton key={i} aria-label={menu.label}>
-            {menu.icon}
-          </IconButton>
-        ))}
-      </div>
+      <div className="bg-green-800 h-[75px]"></div>
     </div>
   );
 };
