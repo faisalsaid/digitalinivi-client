dfca0a6b979b38954226737d90677a2fd52863da
faisalsaid <falsdesain@gmail.com>
2023-12-14 19:53:19 +0900

    chage type privater route navigate to template

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 export default function PrivateRoute({ component: Component, authenticated, ...rest }) {
   if (!authenticated) {
-    return <Navigate to={'/tempalte'} />;
+    return <Navigate to={'/template'} />;
   }
   return <Outlet />;
 }
