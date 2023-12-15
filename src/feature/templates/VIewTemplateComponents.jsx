60ea0f74d941c702a7cc8661c3a595b43e0e04c9
faisalsaid <falsdesain@gmail.com>
2023-12-15 10:33:32 +0900

    Fix template view layout

--- src/feature/templates/VIewTemplateComponents.jsx
+++ src/feature/templates/VIewTemplateComponents.jsx
@@ -4,8 +4,13 @@ import MasterTemplate from './invitationTemplate/MasterTemplate';
 import { colorTheme } from '../../hook/static/themeDetail';
 import THEME_LIST from '../../hook/static/THEME_LIST.json';
 import INVITAION_INFO from '../../hook/static/MOC_INVITATION_DETAIL.json';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 const VIewTemplateComponents = () => {
+  const [viewSetting, setViewSetting] = useState(false);
   const { code } = useParams();
   useEffect(() => {
     const result = THEME_LIST.filter((theme) => theme.code === code);
@@ -24,26 +29,35 @@ const VIewTemplateComponents = () => {
   };
 
   return (
-    <>
-      <div className="p-6  justify-between flex-col gap-2 ">
-        <p>
-          Kode Thema : <span className="uppercase">{code}</span>
-        </p>
-        <div className="flex gap-1 flex-col">
-          <p>Warna Tema :</p>
-          <div className="flex gap-6 items-center">
-            {colorTheme &&
-              colorTheme.map((data, i) => (
-                <div key={data.name} className="flex gap-2 items-center   px-2  rounded-md border" style={{ backgroundColor: data.light, borderColor: data.dark }}>
-                  <input className="cursor-pointer" type="radio" name="theme_color" id={data.name} value={data.name} onChange={() => handleColorSchema(data.name)} />
-                  <label className="cursor-pointer " htmlFor={data.name}>
-                    <span className="capitalize">{data.name}</span>
-                  </label>
-                </div>
-              ))}
+    <div className="relative">
+      <div className="absolute flex flex-col gap-2 right-4 top-4">
+        <Fab onClick={() => navigate('/template')}>
+          <ArrowBackIosIcon />
+        </Fab>
+        <Fab onClick={() => setViewSetting(!viewSetting)} color="info">
+          <SettingsSuggestIcon />
+        </Fab>
+        <div className={`${viewSetting ? 'flex' : 'hidden'} flex-col absolute z-10 bg-white p-3 right-16 top-0 rounded-lg w-32 gap-3 transition-all ease-in-out shadow-md`}>
+          <p className="w-full bg-sky-100 p-2 rounded-md">
+            <span className="uppercase font-medium    ">{code}</span>
+          </p>
+          <div className="flex gap-1 flex-col">
+            <p>Warna:</p>
+            <div className="flex flex-col gap-2 ">
+              {colorTheme &&
+                colorTheme.map((data, i) => (
+                  <div key={data.name} className="flex gap-2 items-center   px-2  rounded-md border" style={{ backgroundColor: data.light, borderColor: data.dark }}>
+                    <input className="cursor-pointer" type="radio" name="theme_color" id={data.name} value={data.name} onChange={() => handleColorSchema(data.name)} />
+                    <label className="cursor-pointer " htmlFor={data.name}>
+                      <span className="capitalize">{data.name}</span>
+                    </label>
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
       </div>
+
       <MasterTemplate
         invitaionTheme={{
           color: colorSchema,
@@ -52,7 +66,7 @@ const VIewTemplateComponents = () => {
         }}
         info={INVITAION_INFO}
       />
-    </>
+    </div>
   );
 };
 
